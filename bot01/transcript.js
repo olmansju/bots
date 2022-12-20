f7666d1c99b96f3ab2434e74bd1df4a03eea3ebf
Justin Olmanson <olmansju@gmail.com>
2022-12-19 23:48:42 -0600

    added a simplified bot01 in preparation for using it w GPT3

--- bot01/transcript.js
+++ bot01/transcript.js
@@ -24,14 +24,21 @@ async function botResponse(responseText){
     setTimeout(baselineBotResponse, 1400);
 }
 
-function baselineBotResponse(){
+function addHexColor(h1, h2) {
+    let hexStr = (parseInt(h1, 16) + parseInt(h2, 16)).toString(16);
+    while (hexStr.length < 6) { hexStr = '0' + hexStr; } // Zero pad.
+    return hexStr;
+}
 
-    let responseGiven = ``;
+function baselineBotResponse(){
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let randomPlus42 = addHexColor(randomColor, '2a2a');
+    let responseGiven = `My mouth is the hex color #${randomColor}! My lips are the hex color #${randomPlus42}!`;
     transcriptArray.push([botName, responseGiven]);
     console.log('bot response: ', responseGiven);
-
     document.getElementById("chatInput").focus();
-
+    document.getElementById("mouth").style.fill = `#${randomColor}`;
+    document.getElementById("mouth").style.stroke = `#${randomPlus42}`;
     buildTranscript();
     document.getElementById("chatInput").disabled = false;
 
