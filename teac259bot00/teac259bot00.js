4a34753121a18be492de4e7ca8e26c20e803d074
olmansju <olmansju@gmail.com>
2023-01-07 15:29:24 -0600

    added a TA bot for an undergraduate course, UI in progress

---
+++ teac259bot00/teac259bot00.js
@@ -0,0 +1,40 @@
+const botName = "TA-259-Bot05";
+let userName = "User";
+let prompt = `In the following conversation, you are ${botName}, a helpful, knowledgeable, and kind Teaching Assistant for TEAC 259, a course on technology integration taken by future teachers and future speech language pathologists.`;
+
+
+async function chatSummary(userID){
+    let mod = "text-curie-001";
+    let temp = 0;
+    let answerLength = "360";
+    let promptCommand = "Summarize this chatlog:";
+    let chatlog = formatArrayIntoModelPrompt(transcriptArray);
+    let responseModel = `1. What is the main topic?\n2. What advice did ${botName} give to ${userName}?\n3. What would be a one sentence summary of the interaction?\n4. What is the sentiment of the conversation?\n5. What is a shorthand of the conversation?\n6. What is a 10 word summary of the conversation?`;
+    let promptEnd = "#Answers#\n1.";
+    let promptForSummary = `${promptCommand}\n\n${chatlog}\n\n${responseModel}\n\n${promptEnd}`;
+    let summaryGiven = await GPT3request(promptForSummary, userID, answerLength, mod, temp);
+    document.getElementById("summary").innerText = `1. ${summaryGiven}`;
+}
+
+async function botResponse(responseText, userID){
+    processLog("botResponse", `passed data:: ${responseText}`);
+    let preppedPrompt = prepPrompt(prompt, transcriptArray);
+    let model = "curie:ft-ilt-tlte-cehs-unl-2023-01-06-21-01-08";
+    let temp = 0.6;
+    callGPT3botUI(preppedPrompt, userID, model, temp);
+}
+
+function prepPrompt(prompt, arrayOfTransaction){
+    let formattedModel = formatArrayIntoModelPrompt(arrayOfTransaction);
+    let theFullPromptAndModel = `${prompt}\n${formattedModel}`;
+    document.getElementById("fullPrompt").innerText = theFullPromptAndModel;
+    return theFullPromptAndModel;
+}
+
+function formatArrayIntoModelPrompt(arrayOfTransaction){
+    let model = "";
+    arrayOfTransaction.forEach(dialoguePair => {
+        model += `\n${dialoguePair[0]}: ${dialoguePair[1]}\n${botName}:`;
+    } );
+    return model;
+}
