542bd0914d950594dcaa7563e9b23eed2d4521c6
olmansju <olmansju@gmail.com>
2023-04-30 18:01:37 -0500

    fixed bugs

--- bot/bot.js
+++ bot/bot.js
@@ -30,12 +30,17 @@ function initiateResponse() {
 
 async function initiateProcess(query, univID){
     let evaluativeArray = [];
+    let sendingArray = [];
+    let sendingPrompt = `You are ${botName}, a PhD advising assistant. Respond to ${userName} in a kind, helpful way.`;
+    let sendingPromptObject = {'role': 'system', 'content': sendingPrompt};
+    sendingArray.push(sendingPromptObject);
     //get embeddings (in the future maybe don't use await as the next part should take longer and so this will be ready before the await returns)
     let theResponseEmbeddings = await getUserResponseEmbeddings(query);
     //establish the nature of question use await
     let prompt = `You are ${botName}, a PhD advising assistant. You are helping evaluate student submitted queries. For each query provide an answer to the following questions: \n QUERY1# on a scale of 0.001 to 0.999 what is the probability that this query is related to PhD program advising (please answer only with a number, no additional text)? \n QUERY2# if the probability in QUERY#1 was greater than .5, what would be an interesting (e.g. useful, witty, amusing) response to this query that would occupy the user while the answer was looked up (in 280 characters or less)? \n QUERY3# if the probability in QUERY#1 was less than .5001, what would be your best answer to this question? \n QUERY4# what clarifying question would you ask of someone asking this query? \n QUERY5# what are two insights you can interpolate from the student's query? \n Prepend your answer to each question with: QUERY1#, QUERY2#, QUERY3#, QUERY4#, and QUERY5# respectively \n`;
     evaluativeArray.push({'role': 'system', 'content': prompt});
     let userObject = {'role': 'user', 'content': query};
+    buildChatBubble(userObject);
     evaluativeArray.push(userObject);
     //document.getElementById("userInput").value = "";
     //document.getElementById("userInput").disabled = true;
@@ -46,19 +51,24 @@ async function initiateProcess(query, univID){
     if (evalObj.q1 > 0.4999){
         //the question is probably about advising: dole out q2, match embeddings
         console.log('Pertinent to advising:', evalObj.q1, 'buy time statement:', evalObj.q2, 'buy time question:', evalObj.q4, 'buy time insights:', evalObj.q5);
-        let top3MatchObjArray = queryPineconeForBestMatch(theResponseEmbeddings, univID); //function not written yet
+        buildChatBubble({'role': botName, 'content': evalObj.q2});
+        let top3MatchObjArray = await queryPineconeForBestMatch(theResponseEmbeddings, univID);
         if (top3MatchObjArray.length > 0 && top3MatchObjArray[0].score > 0.699){
             let contentInfo = {'role': 'system', 'content': `use the following information about ${userName} and their program to respond: ${top3MatchObjArray[0].metadata.naturalText}`};
-            evaluativeArray.push(contentInfo);
+            sendingArray.push(contentInfo);
             let queryObject = {'role': 'user', 'content': query};
-            evaluativeArray.push(queryObject);
-            //----up to here----//
+            sendingArray.push(queryObject);
+            let botFinalResponse = await botResponse(sendingArray, univID);
+            buildChatBubble({'role': botName, 'content': botFinalResponse});
         } else {
-
+            console.log('Middling match score:', top3MatchObjArray[0].score, 'response:', evalObj.q3);
+            buildChatBubble({'role': botName, 'content': 'Hmmm, I might need more information.'});
+            buildChatBubble({'role': botName, 'content': evalObj.q2});
         }
     } else {
         //the question is probably not about advising: use q3 as response
         console.log('Not pertinent to advising:', evalObj.q1, 'response:', evalObj.q3);
+        buildChatBubble({'role': botName, 'content': evalObj.q3});
     }
 /*
         chatArray.push(userObject);
@@ -113,6 +123,12 @@ function extractFloatFromString(str) {
 async function botEvalResponse(chArray, userID){
     let responseObject;
     responseObject = await callGPT35eval(chArray, userID, mod, temp, version);
+    return responseObject;
+}
+
+async function botResponse(chArray, userID){
+    let responseObject;
+    responseObject = await GPT35request(chArray, userID, '1200', mod, temp, version);
     return responseObject;
 }
 
